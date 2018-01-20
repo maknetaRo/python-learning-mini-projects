9e54c21004325008dbb69d8fd9e851e8023b948b
Magda <makneta@gmail.com>
2018-01-20 19:47:16 +0100

    No. 12

---
+++ haikugenerator.py
@@ -0,0 +1,12 @@
+# An easy program to go through lists and create a haiku poem(sort of)
+import random
+word_list1 = ["brunatne", "kochane", "radosne", "żałobne"]
+word_list2 = ["drzewa", "ptaki", "doliny", "obrazy"]
+word_list3 = ["dzisiaj", "wczoraj", "jutro", "kiedyś"]
+word_list4 = ["mokną", "grają", "niosą", "płyną"]
+word_list5 = ["żal", "stos", "szok", "kłos"]
+word_list6 = ["przemijania", "usypiania", "trzepotania", "parowania"]
+
+print(random.choice(word_list1), random.choice(word_list2))
+print(random.choice(word_list3), random.choice(word_list4))
+print(random.choice(word_list5), random.choice(word_list6))
