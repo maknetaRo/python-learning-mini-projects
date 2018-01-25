e09ae1760edccd75fa0de7a34d6846693b50620f
Magda <makneta@gmail.com>
2018-01-25 22:42:57 +0100

    No. 18

--- count_words.py
+++ count_words.py
@@ -4,8 +4,9 @@ file_name = "play_play.txt"
 
 with open(file_name) as file_object:
 	for line in file_object:
-		words = int(line.count(" ")) + 1
-		print(line, words)
+		words = line.split()
+		number = len(words)
+		print(line, words, number)
 
 
 with open(file_name) as file_object:
@@ -16,7 +17,17 @@ for line in lines:
 	play_play += line.strip()
 	play_play = play_play.replace(".", " ")
 
-words = int(play_play.count(" "))
+words = play_play.spli()
+number = len(words)
 print(play_play)
-print("There are {} words.".format(words))
+print("There are {} words.".format(number))
+
+infile = open("C:\\Users\\Magda\\Desktop\\simple\\learning_python.txt", "r")
+data = infile.read()
+num_of_chars = len(data)
+num_of_words = len(data.split())
+num_of_lines = len(data.splitlines())
+
+print(num_of_chars, num_of_words, num_of_lines)
+
 
