7b60c66b0e91474f2355c8a1b22f32deb8653f7a
Magda <makneta@gmail.com>
2018-02-03 13:47:29 +0100

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,7 +6,7 @@ for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
         print("FizzBuzz")
     elif number % 3:
-        print("Fizz)
+        print("Fizz")
     elif number % 5 == 0:
         print("Buzz")
     else:
