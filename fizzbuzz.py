349d0a31a62874c77c74f9c203fee1233eb7c8e6
Magda <makneta@gmail.com>
2018-02-04 22:42:02 +0100

    added function

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,13 +2,19 @@
 # Fizz Buzz is a game for  children to teach them division. Players replace any number divisible by three with the word\
 #  "fizz" and by five with the word "buzz".
 
-for number in range(1, 101):
-    if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
-    elif number % 3:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
-    else:
-        print(number)
+def fizz_buzz():
+    
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+            
+            
+fizz_buzz()
+            
 
